1a1ddea22d2e374caf24c63b1aaca98a1dacfb25
Ehsan Sarshar <ehsan.herai@gmail.com>
2021-10-16 22:44:43 +0430

    initial setup

---
+++ android/app/src/main/java/com/wallet/MainActivity.java
@@ -0,0 +1,8 @@
+package com.wallet;
+
+import com.reactnativenavigation.NavigationActivity;
+
+public class MainActivity extends NavigationActivity {
+
+  
+}
---
+++ index.js
@@ -0,0 +1,21 @@
+/**
+ * @format
+ */
+import {Navigation} from 'react-native-navigation'
+import App from './App'
+Navigation.registerComponent('com.myApp.WelcomeScreen', () => App)
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: 'com.myApp.WelcomeScreen',
+            },
+          },
+        ],
+      },
+    },
+  })
+})
